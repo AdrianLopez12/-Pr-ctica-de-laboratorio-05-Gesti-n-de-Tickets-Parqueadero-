ff2c7f91da2ff4c13717c88d9920325c949f6d8d
AdrianLopez12 <33838397+AdrianLopez12@users.noreply.github.com>
2020-06-22 18:08:37 -0500

    inicialisacion del proyecto

---
+++ src/ec/edu/ups/modelo/Ticket.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.modelo;
+public class Ticket {
+    
+    private int codigo;
+    private int hora;
+    
+    public void holamundo(){
+        System.out.println("hola mundo");
+    }
+    
+    
+}
